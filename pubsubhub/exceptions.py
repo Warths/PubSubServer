4ebc4118061f136f8748137540ad4a3627e5d97f
Warths <pro.warths@gmail.com>
2021-04-06 15:15:33 +0200

    Added custom exceptions

--- pubsubhub/exceptions.py
+++ pubsubhub/exceptions.py
@@ -1,17 +1,26 @@
 class PubSubException(Exception):
     pass
 
+
 class PubSubServerException(PubSubException):
     """ Generic, PubSubHub related"""
     pass
+
 
 class PubSubClientException(PubSubException):
     """ Generic, PubSubClient related """
     pass
 
+
 class PublishError(PubSubServerException):
     """ Raised when a publication fails"""
     pass
+
+
+class SendException(PubSubClientException):
+    """ Raised when PubSubClient could not send its payload"""
+    pass
+
 
 class SubscribeError(PubSubClientException):
     """ Raised when a subscription fails """
